6d322daab195ca9699d0c51cdb598ecc165f3031
akmaljonmordayev <akmaljonmordayev@gmail.com>
2024-03-12 16:49:09 +0500

    created

---
+++ main.js
@@ -0,0 +1,20 @@
+// 1) Ikkita o'zgaruvchi yarating va ularning ichiga son saqlang va bir birini cosole.logda qo'shib chiqaring chiqaring
+
+
+// 2) let a  = '12' shu 12 sonini data typeni o'zgartirib number qilib chiqaring 
+// bering. Yani bu hozir string typeda siz number tyga o'zgartiring
+
+
+// 3) randomli son yasang va shu randomli sonning juft yoki toq ekanligini aniqlab bering
+
+
+// 4) promptdan bitta so'z kiriting agar o'sha so'z Mars so'ziga teng bolsa o'sha so'zning
+//  o'zi chiqsin agar boshqa so'z bo'lsa Mars It school deb chiqsin
+
+
+// 5) bitta array yarating va uning ichiga 5 ta so'z kiriting. Keyin For yordamida o'sha arraydagi 
+// so'zlar ichidan uzunligi 5 va undan kattalarini bohsqa bir arrayga qo'shib chiqaring
+
+
+
+// 6) for orqali birdan 100gacha bolgan sonlarni juft yoki toqqa ajratib chiqaring
